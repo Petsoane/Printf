d2b65b67ac35caefdeab1e30e2ff8be83dd97fbd
Lebogang PETSOANE <lpetsoan@c5r5s5.wethinkcode.co.za>
2020-01-11 08:23:02 +0200

    Trying to norm my shit

--- ft_printf.c
+++ ft_printf.c
@@ -6,28 +6,28 @@
 /*   By: lpetsoan <lpetsoan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/06 14:19:18 by lpetsoan          #+#    #+#             */
-/*   Updated: 2020/01/08 15:28:41 by lpetsoan         ###   ########.fr       */
+/*   Updated: 2020/01/08 15:33:25 by lpetsoan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
 
-int                 ft_printf(const char *str, ...)
+int		ft_printf(const char *str, ...)
 {
-    char            **split;
-    va_list         list;
+	char			**split;
+	va_list			list;
 
-    va_start(list, str);
-    split =ft_strsplit(str, ' ');
-    while(*split)
-    {
+	va_start(list, str);
+	split = ft_strsplit(str, ' ');
+	while (*split)
+	{
 		if (ft_strchr(*split, '%'))
-            process_split(*split, list);
-        else
-            ft_putstr(*split);
-        if (*(split + 1) != NULL)
-            ft_putchar(' ');
-        split++;
-    }
-    return (0);
+			process_split(*split, list);
+		else
+			ft_putstr(*split);
+		if (*(split + 1) != NULL)
+			ft_putchar(' ');
+		split++;
+	}
+	return (0);
 }
--- ft_printf.h
+++ ft_printf.h
@@ -6,36 +6,35 @@
 /*   By: lpetsoan <lpetsoan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/01/06 14:23:02 by lpetsoan          #+#    #+#             */
-/*   Updated: 2020/01/08 15:19:07 by lpetsoan         ###   ########.fr       */
+/*   Updated: 2020/01/08 15:35:36 by lpetsoan         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FTPRINTF_H
-#   define FTPRINTF_H
-#   include <stdlib.h>
-#   include <unistd.h>
-#   include "libft/libft.h"
-#   include <stdarg.h>
+#	define FTPRINTF_H
+#	include <stdlib.h>
+#	include <unistd.h>
+#	include "libft/libft.h"
+#	include <stdarg.h>
 
-typedef struct      s_type
+typedef struct		s_type
 {
-    int             left_just;
-    int             padded;
-}                   t_flags;
+	int				left_just;
+	int				padded;
+}					t_flags;
 
-char                *get_precision(char *split);
-char                *get_minFieldWidth(char *split);
-void                get_flags(char *split, t_flags *flags);
-int                 ft_printf(const char *, ...);
-void                print_char(va_list list);
-void                print_int(char *split, va_list list);
-void                print_unsigned(va_list list);
-void                print_string(va_list list, char *precision);
-void                print_hex(char *split, va_list list);
-void                print_oct(char *split, va_list list);
-void                print_pointer(va_list list);
-void                print_float(va_list list, char *precision);
-void                print_spaces(char *split);
-void                process_split(char *split, va_list list);
-
+char				*get_precision(char *split);
+char				*get_minFieldWidth(char *split);
+void				get_flags(char *split, t_flags *flags);
+int					ft_printf(const char *, ...);
+void				print_char(va_list list);
+void				print_int(char *split, va_list list);
+void				print_unsigned(va_list list);
+void				print_string(va_list list, char *precision);
+void				print_hex(char *split, va_list list);
+void				print_oct(char *split, va_list list);
+void				print_pointer(va_list list);
+void				print_float(va_list list, char *precision);
+void				print_spaces(char *split);
+void				process_split(char *split, va_list list);
 #endif
